254848a75fc5fbb44b426d1f17084e1143ca28f6
Oleksandr Lischynskyi <sashalischinskiy72@gmail.com>
2023-07-11 22:58:53 +0300

    Wrap in useCallback and add navigation in useEffect

--- frontend/src/pages/Sign/SignUpContainer.tsx
+++ frontend/src/pages/Sign/SignUpContainer.tsx
@@ -1,19 +1,28 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
 import SignUp from './SignUp';
 import { authActions } from '@redux/auth/authSlice';
 import { useSelector } from 'react-redux';
 import { RootState, useAppDispatch } from '@redux/store/index';
-import { DOMAIN } from '@constants';
 import { registerUser } from '@redux/auth/authTnunk';
 
 const SignUpContainer = () => {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
 
-  const { setAlert, clearAlert } = authActions;
+  const { clearAlert } = authActions;
   const isError = useSelector((state: RootState) => state.auth.alert.isActive);
+  const isUserRegistered = useSelector(
+    (state: RootState) => state.auth.isRegistered,
+  );
+
+  useEffect(() => {
+    if (isUserRegistered) {
+      setTimeout(() => {
+        navigate('/login');
+      }, 1000);
+    }
+  }, [isUserRegistered, navigate]);
 
   const handleInput = () => {
     // Clear error if user start typing in inputs
@@ -21,23 +30,22 @@ const SignUpContainer = () => {
     dispatch(clearAlert());
   };
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    const dataToSend = {
-      email: data.get('email'),
-      password: data.get('password'),
-      firstName: data.get('firstName'),
-      lastName: data.get('lastName'),
-      username: data.get('username'),
-    };
+  const handleSubmit = useCallback(
+    (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      const data = new FormData(event.currentTarget);
+      const dataToSend = {
+        email: data.get('email'),
+        password: data.get('password'),
+        firstName: data.get('firstName'),
+        lastName: data.get('lastName'),
+        username: data.get('username'),
+      };
 
-    dispatch(registerUser(dataToSend)).then(() => {
-      setTimeout(() => {
-        navigate('/sign-in');
-      }, 1000);
-    });
-  };
+      dispatch(registerUser(dataToSend));
+    },
+    [dispatch],
+  );
 
   return <SignUp {...{ handleSubmit, handleInput }}></SignUp>;
 };
