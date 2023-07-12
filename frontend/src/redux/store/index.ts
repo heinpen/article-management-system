535fc5e440827a33de3ffaf522064086c979ac54
Oleksandr Lischynskyi <sashalischinskiy72@gmail.com>
2023-07-12 15:08:53 +0300

    Add the api middleware that enables useful features of `rtk-query`

--- frontend/src/redux/store/index.ts
+++ frontend/src/redux/store/index.ts
@@ -1,11 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
-import authReducer from '../slices/authSlice';
+import authReducer from '../auth/authSlice';
 import { useDispatch } from 'react-redux';
+import { authApi } from '@services/authApi';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    [authApi.reducerPath]: authApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(authApi.middleware),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
