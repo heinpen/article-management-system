c028cb8a94255f31ff4184aa6191e2cc2bf57d85
Oleksandr Lischynskyi <sashalischinskiy72@gmail.com>
2023-07-13 14:05:45 +0300

    Add form validation

--- frontend/src/components/auth/LoginForm.tsx
+++ frontend/src/components/auth/LoginForm.tsx
@@ -2,62 +2,75 @@ import {
   Box,
   Button,
   Checkbox,
+  FormControl,
   FormControlLabel,
+  FormHelperText,
   Grid,
   TextField,
 } from '@mui/material';
 import Link from '@mui/material/Link';
 import { LoginData } from '@types';
-import { FC, useCallback, useRef } from 'react';
+import { useFormik } from 'formik';
+import { FC } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 
+import * as Yup from 'yup';
 interface LoginFormProps {
   handleSubmit: (data: LoginData) => void;
   reset: () => void;
 }
 
-const LoginForm: FC<LoginFormProps> = ({ handleSubmit, reset }) => {
-  const checkboxRef = useRef() as React.MutableRefObject<HTMLInputElement>;
+const validationSchema = Yup.object({
+  emailOrUsername: Yup.string().required('Email or username is required'),
+  password: Yup.string()
+    .min(8, 'Password should be of minimum 8 characters length')
+    .required('Password is required'),
+});
 
-  const handleInput = () => {
+const LoginForm: FC<LoginFormProps> = ({ handleSubmit, reset }) => {
+  const formik = useFormik({
+    initialValues: {
+      emailOrUsername: '',
+      password: '',
+      isChecked: true,
+    },
+    validationSchema,
+
+    onSubmit: (values) => {
+      console.log(values);
+
+      handleSubmit(values);
+    },
+  });
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     // Clear any error if a user starts typing in inputs
+    formik.handleChange(e);
     reset();
   };
 
-  const handleLocalSubmit = useCallback(
-    (event: React.FormEvent<HTMLFormElement>) => {
-      event.preventDefault();
-      const data = new FormData(event.currentTarget);
-
-      const dataToSend: LoginData = {
-        emailOrUsername: data.get('emailOrUsername'),
-        password: data.get('password'),
-        isChecked: checkboxRef.current?.checked,
-      };
-
-      handleSubmit(dataToSend);
-    },
-    [handleSubmit],
-  );
-
   return (
-    <Box
-      component="form"
-      onSubmit={handleLocalSubmit}
-      noValidate
-      sx={{ mt: 1 }}
-    >
+    <Box component="form" onSubmit={formik.handleSubmit} sx={{ m: 1 }}>
       <TextField
         margin="normal"
         required
         fullWidth
-        type="text"
-        id="email"
+        id="emailOrUsername"
         label="Email Address or Username"
-        name="emailOrUsername"
         autoComplete="email"
         autoFocus
+        type="text"
+        name="emailOrUsername"
         onChange={handleInput}
+        onBlur={formik.handleBlur}
+        value={formik.values.emailOrUsername}
+        error={
+          formik.touched.emailOrUsername &&
+          Boolean(formik.errors.emailOrUsername)
+        }
+        helperText={
+          formik.touched.emailOrUsername && formik.errors.emailOrUsername
+        }
       />
       <TextField
         margin="normal"
@@ -68,14 +81,35 @@ const LoginForm: FC<LoginFormProps> = ({ handleSubmit, reset }) => {
         type="password"
         id="password"
         autoComplete="current-password"
-        onChange={handleInput}
+        value={formik.values.password}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        error={formik.touched.password && Boolean(formik.errors.password)}
+        helperText={formik.touched.password && formik.errors.password}
       />
-      <FormControlLabel
-        control={
-          <Checkbox inputRef={checkboxRef} defaultChecked color="primary" />
-        }
-        label="Remember me"
-      />
+
+      <FormControl
+        error={formik.touched.isChecked && Boolean(formik.errors.isChecked)}
+        component="fieldset"
+        sx={{ mt: 2 }}
+      >
+        <FormControlLabel
+          control={
+            <Checkbox
+              name="isChecked"
+              color="primary"
+              checked={formik.values.isChecked}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+            />
+          }
+          label="Remember me"
+        />
+        {formik.touched.isChecked && formik.errors.isChecked && (
+          <FormHelperText>{formik.errors.isChecked}</FormHelperText>
+        )}
+      </FormControl>
+
       <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
         Login
       </Button>
@@ -90,7 +124,7 @@ const LoginForm: FC<LoginFormProps> = ({ handleSubmit, reset }) => {
             component={RouterLink}
             to="/register"
             variant="body2"
-            onClick={handleInput}
+            onClick={() => reset()}
           >
             {"Don't have an account? Sign Up"}
           </Link>
--- frontend/src/components/auth/RegistrationForm.tsx
+++ frontend/src/components/auth/RegistrationForm.tsx
@@ -8,38 +8,54 @@ import {
 } from '@mui/material';
 import Link from '@mui/material/Link';
 import { RegistrationData } from '@types';
+import { useFormik } from 'formik';
 import { FC, useCallback } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
+import * as Yup from 'yup';
 
 interface RegistrationFormProps {
   handleSubmit: (data: RegistrationData) => void;
   reset: () => void;
 }
 
+const validationSchema = Yup.object({
+  email: Yup.string()
+    .email('Invalid email address')
+    .required('Email is required'),
+  username: Yup.string().required('Username is required'),
+  firstName: Yup.string().required('First name is required'),
+  lastName: Yup.string().required('Last name is required'),
+  password: Yup.string()
+    .required('Password is required')
+    .min(6, 'Password must be at least 6 characters'),
+});
+
 const RegistrationForm: FC<RegistrationFormProps> = ({
   handleSubmit,
   // handleInput,
   reset,
 }) => {
-  const handleLocalSubmit = useCallback(
-    (event: React.FormEvent<HTMLFormElement>) => {
-      event.preventDefault();
-      const data = new FormData(event.currentTarget);
-      const dataToSend: RegistrationData = {
-        email: data.get('email'),
-        password: data.get('password'),
-        firstName: data.get('firstName'),
-        lastName: data.get('lastName'),
-        username: data.get('username'),
-      };
-      console.log(dataToSend);
-      handleSubmit(dataToSend);
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      username: '',
+      firstName: '',
+      lastName: '',
+      password: '',
+      isChecked: true,
     },
-    [handleSubmit],
-  );
+    validationSchema,
 
-  const handleInput = () => {
+    onSubmit: (values) => {
+      console.log(values);
+
+      handleSubmit(values);
+    },
+  });
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     // Clear error if user start typing in inputs
+    formik.handleChange(e);
     reset();
   };
 
@@ -47,11 +63,12 @@ const RegistrationForm: FC<RegistrationFormProps> = ({
     <>
       <Box
         component="form"
-        onSubmit={handleLocalSubmit}
+        onSubmit={formik.handleSubmit}
         noValidate
         sx={{ mt: 1 }}
       >
         <TextField
+          {...formik.getFieldProps('email')}
           margin="normal"
           required
           fullWidth
@@ -61,8 +78,11 @@ const RegistrationForm: FC<RegistrationFormProps> = ({
           autoComplete="email"
           autoFocus
           onChange={handleInput}
+          error={formik.touched.email && Boolean(formik.errors.email)}
+          helperText={formik.touched.email && formik.errors.email}
         />
         <TextField
+          {...formik.getFieldProps('username')}
           margin="normal"
           required
           fullWidth
@@ -71,8 +91,11 @@ const RegistrationForm: FC<RegistrationFormProps> = ({
           name="username"
           autoComplete="username"
           onChange={handleInput}
+          error={formik.touched.username && Boolean(formik.errors.username)}
+          helperText={formik.touched.username && formik.errors.username}
         />
         <TextField
+          {...formik.getFieldProps('firstName')}
           margin="normal"
           required
           fullWidth
@@ -80,8 +103,11 @@ const RegistrationForm: FC<RegistrationFormProps> = ({
           label="First name"
           name="firstName"
           autoComplete="given-name"
+          error={formik.touched.firstName && Boolean(formik.errors.firstName)}
+          helperText={formik.touched.firstName && formik.errors.firstName}
         />
         <TextField
+          {...formik.getFieldProps('lastName')}
           margin="normal"
           required
           fullWidth
@@ -89,8 +115,11 @@ const RegistrationForm: FC<RegistrationFormProps> = ({
           label="Last name"
           name="lastName"
           autoComplete="family-name"
+          error={formik.touched.lastName && Boolean(formik.errors.lastName)}
+          helperText={formik.touched.lastName && formik.errors.lastName}
         />
         <TextField
+          {...formik.getFieldProps('password')}
           margin="normal"
           required
           fullWidth
@@ -99,9 +128,13 @@ const RegistrationForm: FC<RegistrationFormProps> = ({
           type="password"
           id="password"
           autoComplete="new-password"
+          error={formik.touched.password && Boolean(formik.errors.password)}
+          helperText={formik.touched.password && formik.errors.password}
         />
         <FormControlLabel
-          control={<Checkbox value="remember" color="primary" />}
+          control={
+            <Checkbox {...formik.getFieldProps('isChecked')} color="primary" />
+          }
           label="Remember me"
         />
         <Button
@@ -118,7 +151,7 @@ const RegistrationForm: FC<RegistrationFormProps> = ({
               component={RouterLink}
               to="/login"
               variant="body2"
-              onClick={handleInput}
+              onClick={() => reset()}
             >
               {'Have account? Sign In'}
             </Link>
