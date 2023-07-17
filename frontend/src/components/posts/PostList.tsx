13d90b7f239f65b06bd09693e4b19de3d8b3ef13
Oleksandr Lischynskyi <sashalischinskiy72@gmail.com>
2023-07-17 20:53:18 +0300

    Create post list

---
+++ frontend/src/components/posts/PostList.tsx
@@ -0,0 +1,22 @@
+import type { FC } from 'react';
+import PostCard from './PostCard';
+import { PostData } from '@types';
+import { Grid } from '@mui/material';
+
+interface PostListProps {
+  posts: PostData[];
+}
+
+const PostList: FC<PostListProps> = ({ posts }) => {
+  if (posts.length === 0) return <h1>No posts found</h1>;
+  return (
+    <Grid container spacing={2}>
+      {posts.map((post) => (
+        <Grid item xs={12} sm={6} md={3} key={post._id}>
+          <PostCard post={post} />
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
+export default PostList;
