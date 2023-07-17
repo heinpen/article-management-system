b3b9401d7013423624096afa11fec1da97080824
Oleksandr Lischynskyi <sashalischinskiy72@gmail.com>
2023-07-17 20:42:52 +0300

    Refactor Home component and implement pageData state and sort functionality

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,23 +1,73 @@
 import Layout from '@components/Layout/Layout';
 import PostList from '@components/posts/PostList';
-import SkeletonList from '@components/posts/SkeletonList';
-import Search from '@components/ui/Search';
-import { Box, Pagination, Typography } from '@mui/material';
-import { useGetPostsMutation } from '@services/postsApi';
-import { useEffect, type FC } from 'react';
 
-interface HomeProps {}
+import Search from '@components/ui/Search';
+import Sort from '@components/ui/Sort';
+import {
+  Box,
+  CircularProgress,
+  Pagination,
+  Stack,
+  Typography,
+} from '@mui/material';
+import { useGetPostsMutation } from '@services/postsApi';
+import { ChangeEvent, useEffect, useState, type FC } from 'react';
+import useDebounce from '../hooks/useDebounce';
+import AlertWrapper from '@components/ui/AlertWrapper';
+import { PostData } from '@types';
+
+interface SortingValues {
+  label: string;
+  value: string;
+}
+
+interface PageData {
+  totalPages: number;
+  sortingValues: SortingValues[];
+  posts: PostData[];
+}
 
 const Home: FC = () => {
   const [trigger, response] = useGetPostsMutation();
   const { data, error, isLoading } = response;
 
-  useEffect(() => {
-    trigger(1);
-    console.log('render');
-  }, [trigger]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const debouncedSearchTerm = useDebounce(searchTerm, 500);
 
-  console.log(data);
+  const [pageData, setPageData] = useState<PageData>({
+    totalPages: 0,
+    sortingValues: [],
+    posts: [],
+  });
+
+  const handleSearch = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSort = (value: string) => {
+    trigger({ pageId: 1, searchTerm: debouncedSearchTerm, sortValue: value });
+  };
+
+  const handlePagination = (event: ChangeEvent<unknown>, n: number) => {
+    trigger({ pageId: n, searchTerm: debouncedSearchTerm });
+    window.scrollTo(0, 0);
+  };
+
+  useEffect(() => {
+    if (data) {
+      setPageData({
+        totalPages: data.pagination.totalPages,
+        sortingValues: data.sortingValues,
+        posts: data.posts,
+      });
+    }
+  }, [data, setPageData]);
+
+  // Perform the search or update based on the debounced value
+  useEffect(() => {
+    trigger({ pageId: 1, searchTerm: debouncedSearchTerm });
+  }, [debouncedSearchTerm, trigger]);
+
   return (
     <Layout>
       <Typography
@@ -26,22 +76,40 @@ const Home: FC = () => {
         color="inherit"
         align="center"
         noWrap
-        sx={{ flex: 1, my: 2 }}
+        sx={{ my: 2 }}
       >
         Posts
       </Typography>
-      <Box sx={{ my: 2 }}>
-        <Search trigger={trigger} />
+      <Stack
+        direction="row"
+        spacing={2}
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Stack direction="row" spacing={2} sx={{ my: 2 }}>
+          <Search handleSearch={handleSearch} />
+          <AlertWrapper isLoading={isLoading} error={error}></AlertWrapper>
+        </Stack>
+        <Box sx={{ my: 2 }}>
+          <Sort
+            handleSort={handleSort}
+            sortingValues={pageData.sortingValues}
+          />
+        </Box>
+      </Stack>
+      <Box sx={{ flex: 1 }}>
+        {pageData.posts && <PostList posts={pageData.posts} />}
       </Box>
-      {data && <PostList posts={data.posts} />}
       <Pagination
-        count={data?.pagination.totalPages}
+        count={pageData.totalPages}
         variant="outlined"
         shape="rounded"
-        onChange={(e, n) => {
-          trigger(n);
+        onChange={handlePagination}
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          my: 2,
         }}
-        sx={{ display: 'flex', justifyContent: 'center', my: 2 }}
       />
     </Layout>
   );
