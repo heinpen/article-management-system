a959713e00951fddc14ace4821b5e29e658f5b7d
Oleksandr Lischynskyi <sashalischinskiy72@gmail.com>
2023-07-19 18:43:29 +0300

    Refactor code and add post creation

--- frontend/src/pages/AdminDashboard.tsx
+++ frontend/src/pages/AdminDashboard.tsx
@@ -1,161 +1,85 @@
-// import Layout from '@components/Layout/Layout';
-// import AlertWrapper from '@components/ui/AlertWrapper';
-// import Search from '@components/ui/Search';
-// import Sort from '@components/ui/Sort';
-// import { Typography, Stack, Box, Pagination } from '@mui/material';
-// import { useGetPostsMutation } from '@services/postsApi';
-// import { PostData } from '@types';
-// import { ChangeEvent, useEffect, useState, type FC } from 'react';
-// import useDebounce from '@hooks/useDebounce';
-// import AdminPostsList from '@components/posts/AdminPostList';
-
-// interface SortingValues {
-//   label: string;
-//   value: string;
-// }
-
-// interface PageData {
-//   totalPages: number;
-//   sortingValues: SortingValues[];
-//   posts: PostData[];
-// }
-
-// const AdminDashboard: FC = () => {
-//   const [getPosts, response] = useGetPostsMutation();
-//   const { data, error, isLoading } = response;
-
-//   const [searchTerm, setSearchTerm] = useState('');
-//   const debouncedSearchTerm = useDebounce(searchTerm, 500);
-
-//   const [pageData, setPageData] = useState<PageData>({
-//     totalPages: 0,
-//     sortingValues: [],
-//     posts: [],
-//   });
-
-//   const handleSearch = (event: ChangeEvent<HTMLInputElement>) => {
-//     setSearchTerm(event.target.value);
-//   };
-
-//   const handleSort = (value: string) => {
-//     getPosts({ pageId: 1, searchTerm: debouncedSearchTerm, sortValue: value });
-//   };
-
-//   const handlePagination = (event: ChangeEvent<unknown>, n: number) => {
-//     getPosts({ pageId: n, searchTerm: debouncedSearchTerm });
-//     window.scrollTo(0, 0);
-//   };
-
-//   useEffect(() => {
-//     if (data) {
-//       setPageData({
-//         totalPages: data.pagination.totalPages,
-//         sortingValues: data.sortingValues,
-//         posts: data.posts,
-//       });
-//     }
-//   }, [data, setPageData]);
-
-//   // Perform the search or update based on the debounced value
-//   useEffect(() => {
-//     getPosts({ pageId: 1, searchTerm: debouncedSearchTerm });
-//   }, [debouncedSearchTerm, getPosts]);
-
-//   return (
-//     <Layout>
-//       <Typography
-//         component="h2"
-//         variant="h5"
-//         color="inherit"
-//         align="center"
-//         noWrap
-//         sx={{ my: 2 }}
-//       >
-//         Posts
-//       </Typography>
-//       <Stack
-//         direction="row"
-//         spacing={2}
-//         justifyContent="space-between"
-//         alignItems="center"
-//       >
-//         <Stack direction="row" spacing={2} sx={{ my: 2 }}>
-//           <Search handleSearch={handleSearch} />
-//           <AlertWrapper isLoading={isLoading} error={error}></AlertWrapper>
-//         </Stack>
-//         <Box sx={{ my: 2 }}>
-//           <Sort
-//             handleSort={handleSort}
-//             sortingValues={pageData.sortingValues}
-//           />
-//         </Box>
-//       </Stack>
-//       <Box sx={{ flex: 1 }}>
-//         {pageData.posts && <AdminPostsList posts={pageData.posts} />}
-//       </Box>
-//       <Pagination
-//         count={pageData.totalPages}
-//         variant="outlined"
-//         shape="rounded"
-//         onChange={handlePagination}
-//         sx={{
-//           display: 'flex',
-//           justifyContent: 'center',
-//           my: 2,
-//         }}
-//       />
-//     </Layout>
-//   );
-// };
-// export default AdminDashboard;
-
 import Layout from '@components/Layout/Layout';
-import PostList from '@components/posts/PostList';
+import AdminModal from '@components/posts/AdminModal';
 
+import AdminPostsList from '@components/posts/AdminPostList';
+import AlertWrapper from '@components/ui/AlertWrapper';
 import Search from '@components/ui/Search';
 import Sort from '@components/ui/Sort';
-import {
-  Box,
-  CircularProgress,
-  Pagination,
-  Stack,
-  Typography,
-} from '@mui/material';
-import { useGetPostsQuery } from '@services/postsApi';
-import { ChangeEvent, useEffect, useState, type FC } from 'react';
 import useDebounce from '@hooks/useDebounce';
-import AlertWrapper from '@components/ui/AlertWrapper';
-import { PostData } from '@types';
-import AdminPostsList from '@components/posts/AdminPostList';
-
-interface SortingValues {
-  label: string;
-  value: string;
-}
-
-interface PageData {
-  totalPages: number;
-  sortingValues: SortingValues[];
-  posts: PostData[];
-}
+import { Box, Button, Pagination, Stack, Typography } from '@mui/material';
+import {
+  useCreatePostMutation,
+  useDeletePostMutation,
+  useGetPostsQuery,
+  useUpdatePostMutation,
+} from '@services/postsApi';
+import { ModalData, PostData, RequestData } from '@types';
+import { ChangeEvent, useEffect, useState, type FC } from 'react';
 
 const AdminDashboard: FC = () => {
-  const [requestData, setRequestData] = useState({
+  const [requestData, setRequestData] = useState<RequestData>({
     page: 1,
     search: '',
     sort: '',
   });
   const { data, error, isLoading, isFetching } = useGetPostsQuery(requestData);
 
-  console.log(data);
   const [searchTerm, setSearchTerm] = useState('');
   const debouncedSearchTerm = useDebounce(searchTerm, 500);
 
-  const [pageData, setPageData] = useState<PageData>({
-    totalPages: 0,
-    sortingValues: [],
-    posts: [],
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const [deletePost] = useDeletePostMutation();
+  const [updatePost] = useUpdatePostMutation();
+  const [createPost] = useCreatePostMutation();
+
+  const [modalData, setModalData] = useState<ModalData>({
+    post: null,
+    isCreate: false,
+    isUpdate: false,
   });
+  const { isCreate, isUpdate, post } = modalData;
+
+  // Handle modal
+  const handleSubmit = (values: { title: string; content: string }) => {
+    if (isCreate) {
+      createPost({
+        data: values,
+        requestData,
+      });
+    } else if (isUpdate && post) {
+      updatePost({
+        id: post._id,
+        data: values,
+        requestData,
+      });
+    }
+    setModalIsOpen(false);
+  };
+
+  const handleModalClose = () => {
+    setModalIsOpen(false);
+  };
+
+  const handleDelete = (id: string) => {
+    deletePost({ id, requestData });
+  };
+
+  const handleCreate = () => {
+    setModalData((prev) => ({ ...prev, isUpdate: false, isCreate: true }));
+
+    setTimeout(() => {
+      setModalIsOpen(true);
+    }, 100);
+  };
+
+  const handleUpdate = (post: PostData) => {
+    setModalData({ post, isUpdate: true, isCreate: false });
+
+    setTimeout(() => {
+      setModalIsOpen(true);
+    }, 100);
+  };
 
   const handleSearch = (event: ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value);
@@ -170,25 +94,15 @@ const AdminDashboard: FC = () => {
     window.scrollTo(0, 0);
   };
 
-  useEffect(() => {
-    if (data) {
-      setPageData({
-        totalPages: data.pagination.totalPages,
-        sortingValues: data.sortingValues,
-        posts: data.posts,
-      });
-    }
-  }, [data, setPageData]);
-
   // Perform the search or update based on the debounced value
   useEffect(() => {
-    console.log(debouncedSearchTerm);
     setRequestData((prev) => ({
       ...prev,
       page: 1,
       search: debouncedSearchTerm,
     }));
   }, [debouncedSearchTerm]);
+  console.log('render');
 
   return (
     <Layout>
@@ -215,18 +129,35 @@ const AdminDashboard: FC = () => {
             error={error}
           ></AlertWrapper>
         </Stack>
-        <Box sx={{ my: 2 }}>
-          <Sort
-            handleSort={handleSort}
-            sortingValues={pageData.sortingValues}
-          />
-        </Box>
+        <Stack direction="row" spacing={2}>
+          <Box>
+            <Button variant="contained" onClick={handleCreate}>
+              Add post
+            </Button>
+          </Box>
+
+          <Box>
+            <Sort handleSort={handleSort} sortingValues={data?.sortingValues} />
+          </Box>
+        </Stack>
       </Stack>
       <Box sx={{ flex: 1 }}>
-        {pageData.posts && <AdminPostsList posts={pageData.posts} />}
+        {data?.posts && (
+          <AdminPostsList
+            posts={data?.posts}
+            handleDelete={handleDelete}
+            handleUpdate={handleUpdate}
+          />
+        )}
+        <AdminModal
+          isOpen={modalIsOpen}
+          handleClose={handleModalClose}
+          handleSubmit={handleSubmit}
+          modalData={modalData}
+        ></AdminModal>
       </Box>
       <Pagination
-        count={pageData.totalPages}
+        count={data?.pagination.totalPages}
         variant="outlined"
         shape="rounded"
         onChange={handlePagination}
