13547356e6083f996e0f83b6c809387290c3521d
Oleksandr Lischynskyi <sashalischinskiy72@gmail.com>
2023-07-21 12:13:55 +0300

    Navigate user to main page on success login

--- frontend/src/pages/auth/Login.tsx
+++ frontend/src/pages/auth/Login.tsx
@@ -1,13 +1,21 @@
 import { useLoginUserMutation } from '@services/authApi';
 import type { FC } from 'react';
-import AlertWrapper from '@components/auth/AlertWrapper';
+import AlertWrapper from '@components/ui/AlertWrapper';
 import AuthWrapper from '@components/auth/AuthWrapper';
 import LoginForm from '@components/auth/LoginForm';
 import AuthHeader from '@components/auth/AuthHeader';
+import { useNavigate } from 'react-router-dom';
 
 const Login: FC = () => {
+  const navigate = useNavigate();
+
   const [triggerLogin, response] = useLoginUserMutation();
-  const { data, error, isLoading, reset } = response;
+  const { data, error, isLoading, isSuccess, reset } = response;
+  if (isSuccess) {
+    setTimeout(() => {
+      navigate('/');
+    }, 1000);
+  }
   return (
     <AuthWrapper>
       <AuthHeader title="Login" />
