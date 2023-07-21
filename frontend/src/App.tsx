63c113a1cc5928af4f58bc75d47eb643ef88887d
Oleksandr Lischynskyi <sashalischinskiy72@gmail.com>
2023-07-21 12:03:17 +0300

    Provide router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,13 @@
 import { CssBaseline } from '@mui/material';
 
-import { Suspense, lazy } from 'react';
-import { useSelector } from 'react-redux';
-import { RootState } from './redux/store';
-
-const ProtectedRoutes = lazy(
-  () => import('./components/Routers/ProtectedRouter'),
-);
-const LoginRoutes = lazy(() => import('./components/Routers/LoginRouter'));
+import { RouterProvider } from 'react-router-dom';
+import { router } from './router/AppRouter';
 
 const App = () => {
-  const isAuth = useSelector((state: RootState) => state.auth.isAuth);
-
   return (
     <>
       <CssBaseline />
-      <Suspense fallback={<div>Loading...</div>}>
-        {isAuth ? <ProtectedRoutes /> : <LoginRoutes />}
-      </Suspense>
+      <RouterProvider router={router} />
     </>
   );
 };
