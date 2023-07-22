e5eef0bdf44e8a1ecc3e375d1bed8f12dd3fdb83
Oleksandr Lischynskyi <sashalischinskiy72@gmail.com>
2023-07-22 16:04:21 +0300

    Add cookie and token expiration extension

--- backend/src/api/middleware/auth.ts
+++ backend/src/api/middleware/auth.ts
@@ -1,4 +1,4 @@
-import { RequestHandler } from 'express';
+import { CookieOptions, RequestHandler } from 'express';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 
 const apiAuthUser: RequestHandler = async (req, res, next) => {
@@ -11,6 +11,19 @@ const apiAuthUser: RequestHandler = async (req, res, next) => {
     if (!JWT_TOKEN) throw new Error('Token is missing');
 
     const { email } = jwt.verify(JWT_TOKEN, SECRET_KEY) as JwtPayload;
+
+    const cookieOptions: CookieOptions = {
+      httpOnly: true,
+      sameSite: 'none',
+      secure: true,
+      expires: new Date(Date.now() + 1000 * 60 * 60 * 12 * 7),
+    };
+
+    const newToken = jwt.sign({ email }, SECRET_KEY, {
+      expiresIn: '7d',
+    });
+
+    res.cookie('JWT_TOKEN', newToken, cookieOptions);
 
     res.locals.email = email as string;
     next();
