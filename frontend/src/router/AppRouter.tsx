2be8dd57a50a88ff3381ec985c42b4b6aa435e05
Oleksandr Lischynskyi <sashalischinskiy72@gmail.com>
2023-07-24 12:51:19 +0300

    Add code splitting

--- frontend/src/router/AppRouter.tsx
+++ frontend/src/router/AppRouter.tsx
@@ -1,3 +1,4 @@
+import { lazy, Suspense } from 'react';
 import {
   createBrowserRouter,
   createRoutesFromElements,
@@ -5,24 +6,54 @@ import {
   Outlet,
   Route,
 } from 'react-router-dom';
-import AdminDashboard from '../pages/AdminDashboard';
-import Login from '../pages/auth/Login';
-import Registration from '../pages/auth/Registration';
-import Posts from '../pages/Posts';
 import RequireAuth from './RequireAuth';
+
+const LazyPosts = lazy(() => import('../pages/Posts'));
+
+const LazyLogin = lazy(() => import('../pages/auth/Login'));
+const LazyRegistration = lazy(() => import('../pages/auth/Registration'));
+const LazyAdminDashboard = lazy(() => import('../pages/AdminDashboard'));
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Outlet />}>
       {/* Public routes */}
       <Route index element={<Navigate to="posts" />} />
-      <Route path="posts" element={<Posts />} />
+      <Route
+        path="posts"
+        element={
+          <Suspense fallback={<div>Loading...</div>}>
+            <LazyPosts />
+          </Suspense>
+        }
+      />
 
-      <Route path="login" element={<Login />} />
-      <Route path="registration" element={<Registration />} />
+      <Route
+        path="login"
+        element={
+          <Suspense fallback={<div>Loading...</div>}>
+            <LazyLogin />
+          </Suspense>
+        }
+      />
+      <Route
+        path="registration"
+        element={
+          <Suspense fallback={<div>Loading...</div>}>
+            <LazyRegistration />
+          </Suspense>
+        }
+      />
       {/* Private routes */}
       <Route element={<RequireAuth allowedRole={'admin'} />}>
-        <Route path="admin" element={<AdminDashboard />} />
+        <Route
+          path="admin"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <LazyAdminDashboard />
+            </Suspense>
+          }
+        />
       </Route>
       {/* 404 */}
       <Route path="*" element={<Navigate to="posts" />} />
